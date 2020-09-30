0d868cd6624f4792273cb419a010a95f02e6618e
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-10-01 00:50:04 +0530

    yaml config file

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+import yaml
 import argparse
 
-parser = argparse.ArgumentParser(description='Apply filters')
+parser = argparse.ArgumentParser(description='Set up Config File')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config_path', type=str, default="", required= True, help='Location of current config file')
-parser.add_argument('--dataset_csvpath', type=str, required=True, default="./", help='Location to data csv file')
+parser.add_argument('--imagePath', type=str, default="", required= False, help='Location of image')
+parser.add_argument('--matrixPath', type=str, default="", required = False , help='Location of file containing matrix')
+parser.add_argument('--s', type=str, required = False , help='Filters in spatial domain separated by commas')
+parser.add_argument('--f', type=str, required = False , help='Filters in frequency domain separated by commas')
+
 
 args = parser.parse_args()
-print("Config Path", args.config_path)
-print("Dataset CSV Path", args.dataset_csvpath)
+
+#Updated Configuration File
+with open("configDefault.yaml") as f:
+    cfg = yaml.load(f, Loader = yaml.FullLoader)
+
+if bool(args.imagePath) is False  and bool(args.matrixPath) is False:
+    print("Path must be specified")
+else:
+    if args.imagePath:
+        cfg["imagePath"] = args.imagePath
+    if args.matrixPath:
+        cfg["matrixPath"] = args.matrixPath
+    if args.s:
+        filterList = args.s.split(",")
+        for i in filterList:
+            cfg["spatial"][i] = True
+#print(filterList)
+with open('config.yaml', "w") as f:
+    yaml.dump(cfg, f)
+
+
