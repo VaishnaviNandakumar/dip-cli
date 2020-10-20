36a96020409f1ee5509aeddc8d9ba1b627bbb3e8
Vaishnavi Nandakumar <vaishoo.nk@gmail.com>
2020-10-21 01:52:04 +0530

    added exceptions

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ from dataLoaderClass import dataLoader
 from filters.spatialFilters import spatialFilterClass
 from filters.frequencyFilters import frequencyFilterClass
 
-#python main.py --p C:\Users\ACER\Desktop\dip-cli\samples\sample.png --dim 300x300 --s laplacian
-#python main.py --p C:\Users\ACER\Desktop\dip-cli\samples\sample.png --dim 300x300 --f ideal
-
-#Parse Arguments
-parser = argparse.ArgumentParser(description='Set up Config File')
+parser = argparse.ArgumentParser(description='Set up Config File')   
 parser = argparse.ArgumentParser()
 parser.add_argument('--p', type=str, default="", required= False, help='Path to Image/Text file')
 parser.add_argument('--dim', type=str, default="", required= False, help='Dimensions of Img/matrix')
@@ -20,39 +16,42 @@ parser.add_argument('--n', required = False , help='Store n value for frequency 
 parser.add_argument('--save', required = False , action='store_true', help='To save output file or not')
 args = parser.parse_args()
 
-
-
-
 def main():
     with open("config\configDefault.yaml") as f:          
         cfg = yaml.load(f, Loader = yaml.FullLoader)
 
     if bool(args.p) is False:
         print("Path must be specified")
-
     else:
         cfg["path"] = args.p                              
-        kind = magic.from_file(cfg["path"], mime = True)  #Determine file format
-        if  kind.find("image")!=-1:
-            cfg["type"] = "image"
-        elif kind.find("text")!=-1:
-            cfg["type"] = "text"
-        else:
+        try :
+            kind = magic.from_file(cfg["path"], mime = True)  #Determine file format
+            if  kind.find("image")!=-1:
+                cfg["type"] = "image"
+            elif kind.find("text")!=-1:
+                cfg["type"] = "text"
+        except FileNotFoundError:
             print("Invalid file format")
+            return
 
-        height, width = args.dim.split("x")
-        cfg["height"] = int(height)
-        cfg["width"] =  int(width)
+        try:
+            if args.dim:
+                height, width = args.dim.split("x")
+                cfg["height"] = int(height)
+                cfg["width"] =  int(width)
+        except (TypeError,ValueError):
+            print("Incorrect Dimensions")
+            return
 
         if args.s:
             filterList = args.s.split(",")
             for i in filterList:
                 cfg["spatial"][i] = True
-        if args.f:
+        elif args.f:
             filterList = args.f.split(",")
             for i in filterList:
                 cfg["frequency"][i] = True
-        
+         
         if args.save:
             cfg["save"] = True
         
@@ -66,7 +65,7 @@ def main():
         yaml.dump(cfg, f)
         
     x = dataLoader(cfg)
-    imgR = x.execute()
+    x.execute()
 
 
 if __name__ == "__main__":
